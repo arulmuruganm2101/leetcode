7797cbb6d3b0d3f92e69ab64d42d852f6ee3e2f2
arulmuruganm2101 <arulmuruganm104@gmail.com>
2026-03-08 13:00:15 +0000

    Sync LeetCode submission Runtime - 2 ms (99.78%), Memory - 46.6 MB (61.69%)

---
+++ 0013-roman-to-integer/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+        int pre=0;
+        int curr;
+        int total=0;
+        for (int i=s.length()-1;i>=0;i--)
+        {
+            curr=switch(s.charAt(i))
+            {
+                case 'I'-> 1;
+                case 'V'-> 5;
+                case 'X'-> 10;
+                case 'L'-> 50;
+                case 'C'-> 100;
+                case 'D'-> 500;
+                case 'M'-> 1000;
+                default ->0;
+            };
+            if (curr<pre)
+            {
+                total-=curr;
+            }
+            else
+            {
+                total+=curr;
+            }
+            pre=curr;
+        }
+        return total;
+    }
+}
